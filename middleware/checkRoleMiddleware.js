8edf69f21032969ec942b391f2097a7d60d28d49
Constantin <konstantinkspb@gmail.com>
2021-05-25 17:32:19 +0300

    finished base level of backend

--- middleware/checkRoleMiddleware.js
+++ middleware/checkRoleMiddleware.js
@@ -1,18 +1,23 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = function (req, res, next) {
-    if (req.method === "OPTIONS") {
-        next();
-    };
-    try {
-        const token = req.headers.authorization.split(' ')[1]
-        if (!token) {
-            return res.status(401).json({message: "You are not authorized!"});
+module.exports = function(role) {
+    return function (req, res, next) {
+        if (req.method === "OPTIONS") {
+            next();
         };
-        const decoded = jwt.verify(token, process.env.SECRET_KEY);
-        req.user = decoded;
-        next();
-    } catch (e) {
-        res.status(401).json({message: "You are not authorized!"});
-    }
+        try {
+            const token = req.headers.authorization.split(' ')[1]
+            if (!token) {
+                return res.status(401).json({message: "You are not authorized!"});
+            };
+            const decoded = jwt.verify(token, process.env.SECRET_KEY);
+            if(decoded.role !== role) {
+                return res.status(403).json({message: "Access denied!"});
+            }
+            req.user = decoded;
+            next();
+        } catch (e) {
+            res.status(401).json({message: "You are not authorized!"});
+        }
+    };
 };
